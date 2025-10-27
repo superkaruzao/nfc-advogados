9c8328fc6832327ff19f0337471a7233664908a3
superkaruzao <79375155+superkaruzao@users.noreply.github.com>
2025-10-27 20:52:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -356,3 +356,30 @@ const fadeObserver = new IntersectionObserver(
 
 const footer = document.querySelector('.footer');
 if (footer) fadeObserver.observe(footer);
+
+var swiper = new Swiper(".swiper", {
+    loop: true,
+    grabCursor: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 18
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 18
+        },
+        1188: {
+            slidesPerView: 3,
+            spaceBetween: 24
+        }
+    }
+});
